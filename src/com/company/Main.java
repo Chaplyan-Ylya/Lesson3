bc621b5462bc185284ec82af66c6e97c8d0f5690
Chaplyan-Ylya <kartinkach@gmail.com>
2020-03-20 20:09:41 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte a = 127;
+        short s = 1270;
+        int d = 222222222;
+        long f = 1_000_000_000_000L;
+        double g = 1.25;
+        float h = 145.268F;
+        char j = '*';
+        boolean k = true;
+        short q = a;
+
+        int w = q;
+        long e = w;
+        System.out.println(e);
+        double r = d;
+		System.out.println(r);
+		float t = s;
+		double y = t;
+		System.out.println(y);
+		int u = j;
+		System.out.println(u);
+
+
+
+    }
+}
---
+++ src/com/company/Main4.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main4 {
+    public static void main(String[] args) {
+        int a=254;
+        System.out.println(a%2==0?"четное":"нечетное");
+
+
+
+
+
+
+    }
+}
